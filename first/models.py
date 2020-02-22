ee988875d5fd653b587e84e519c9e2777b3f4c45
kwonsen <kwonhyunwoo@likelion.org>
2020-02-22 18:14:22 +0900

    Post 모델 필드 추가 및 Model validation예시

--- first/models.py
+++ first/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
+
+
+# model validation 예시
+def validate_text(value):
+    if '욕' in value:
+        raise ValidationError('금지된 말이 들어가 있습니다.')
+    return value
 
 
 class Post(models.Model):
     # 필수 필드이며 외래키 이므로 migrations 시 default 값을 지정해야 한다. ex) 1
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    text = models.TextField()
+    text = models.TextField(validators=[validate_text])
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-id',)  # 역순 정렬
 
 
 class ContentPost(models.Model):
