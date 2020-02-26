7cae5816c63f6a77841b5de1f154066e33e3e403
kwonsen <kwonhyunwoo@likelion.org>
2020-02-26 18:15:07 +0900

    특정 필드값 변경하여 사용하기(serializer)

--- perform/serializers.py
+++ perform/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer, ReadOnlyField, CharField
+from rest_framework.serializers import (ModelSerializer, ReadOnlyField,
+                                        CharField, SerializerMethodField)
 from .models import PerformPost
 
 
@@ -7,8 +8,13 @@ class PerformPostSerializer(ModelSerializer):
     author_username = ReadOnlyField(source='author.username')
     maple = CharField(allow_blank=True, max_length=100, source='text')
 
+    # 특정 필드 값 변경해서 사용하기
+    test = SerializerMethodField(read_only=True)
+
+    def get_test(self, obj):
+        return obj.text + obj.text
+
     class Meta:
         model = PerformPost
-        read_only_fields = ('ip', 'created', 'updated', 'maple')
+        read_only_fields = ('ip', 'created', 'updated', 'maple', 'test')
         fields = ('text', 'author_username') + read_only_fields
-
